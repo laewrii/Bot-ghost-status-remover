0b34a4d1c7e3a24997d2d1b064ec9a965c8cf913
laewrii <fortexservicess@gmail.com>
2024-11-28 13:58:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["Listening to Laewri x Tyxo", "Playing Laewri x Tyxo"];
+const statusTypes = [ 'streaming', 'streaming'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
